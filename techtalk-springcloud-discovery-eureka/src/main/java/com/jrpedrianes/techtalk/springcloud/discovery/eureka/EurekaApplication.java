a29770d09ba0af26767bd0a5f696a6748611caee
Jorge Rodríguez Pedrianes <jorge.rodriguez@system73.com>
2017-08-04 15:05:42 +0100

    update versions

--- techtalk-springcloud-discovery-eureka/src/main/java/com/jrpedrianes/techtalk/springcloud/discovery/eureka/EurekaApplication.java
+++ techtalk-springcloud-discovery-eureka/src/main/java/com/jrpedrianes/techtalk/springcloud/discovery/eureka/EurekaApplication.java
@@ -1,13 +1,13 @@
 package com.jrpedrianes.techtalk.springcloud.discovery.eureka;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+// import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
-@EnableAdminServer
+// @EnableDiscoveryClient
+// @EnableAdminServer
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
--- techtalk-springcloud-service-client/src/main/java/com/jrpedrianes/techtalk/springcloud/DemoApplication.java
+++ techtalk-springcloud-service-client/src/main/java/com/jrpedrianes/techtalk/springcloud/DemoApplication.java
@@ -3,9 +3,11 @@ package com.jrpedrianes.techtalk.springcloud;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @EnableDiscoveryClient
 @SpringBootApplication
